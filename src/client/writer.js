c7393307764bde5e1dfcf8829e8e2c68d4875ac0
Nick George <nick@njpg.me>
2014-10-02 21:19:00 -0700

    adds writer

---
+++ src/client/writer.js
@@ -0,0 +1,29 @@
+goog.provide('Writer');
+
+/**
+ * @constructor
+ * @struct
+ */
+Writer = function(length) {
+  this.offset = 0;
+  this.buffer = new ArrayBuffer(length);
+  this.byteView = new DataView(this.buffer);
+};
+
+Writer.prototype.writeInt8 = function(n) {
+  this.byteView.setInt8(this.offset, n);
+  this.offset++;
+};
+
+Writer.prototype.writeInt16 = function(n) {
+  this.byteView.setInt16(this.offset, n);
+  this.offset += 2;
+};
+
+
+Writer.prototype.writeString = function(str) {
+  for (var i = 0, length = str.length; i < length; i++) {
+    this.writeInt8(str.charCodeAt(i));
+  }
+};
+
